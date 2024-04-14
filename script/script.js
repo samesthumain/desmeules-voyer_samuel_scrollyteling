2794ceea88de7ea805930c0ece7e0eeb8e5a08d6
Samuel Desmeules-Voyer <112189796+samesthumain@users.noreply.github.com>
2024-04-14 04:21:00 -0400

    setup_initial

---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+//appel à l'action
+gsap.fromTo('.appelScroll', {scaleY: 1}, {scaleY: 3, repeat:-1, yoyo:true, ease:"none", duration:1.3});
+
+gsap.fromTo('.appelPoint', {y: "-200%"}, {y: "200%", repeat:-1, ease:"power1.out", duration:1});
+
+gsap.fromTo('.vaisseau', {x: "0%", y: "200%", scale: 1.5}, {x: "700%", y: "-100%", scale: 0.5, ease:"none", duration:80});
