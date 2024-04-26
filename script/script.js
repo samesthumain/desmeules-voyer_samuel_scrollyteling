c05cb01fb33796636f8dc6716840511a9dbfbaaf
Samuel Desmeules-Voyer <samueldv47@gmail.com>
2024-04-26 14:39:34 -0400

    avancé chapitre 1 et 2

--- script/script.js
+++ script/script.js
@@ -58,16 +58,58 @@ gsap.timeline({scrollTrigger: {
     pin:true,
     markers:true,
     start: 'center center',
-    end:'+=3000',
+    end:'+=30000',
     trigger:'#chapitre1',
 }})
-    //.to(".bnc", { duration: 1, opacity: 1, ease: "none", delay: 0.1 })
-    .to(".num-chap-1", { text: 'Chapitre 1:', ease: "none", delay: 0.1 })
+    //intro
+    .to(".num-chap-1", { duration: 1, text: 'Chapitre 1:', ease: "none", delay: 0.1 })
     .to(".nom-chap-1", { duration: 1, text: "Intrusion", ease: "none", delay: 0.1 })
     .to(".num-chap-1", { duration: 1.5, opacity: 0, ease: "none", delay: 1 })
     .to(".nom-chap-1", { duration: 1.5, opacity: 0, ease: "none", delay: -1.5 })
     .to(".bnc1", { duration: 1.5, opacity: 0, ease: "none", delay: -1.5 })
+
+    //action
+    //sprite cours
+    .from(".ss-j-ch1", { x: "800%", ease: "power1.out", })
+
+    //paroles
+    .to(".ch1-line1", { text: "Ici Jeremiah,", ease: "none", delay: 0.1 })
+    .to(".ch1-line2", { text: "23ieme escouade de reconnaissance", ease: "none" })
+
+    .to(".ch1-line1", { duration: 0, text: "", ease: "none", delay: 0.1 })
+    .to(".ch1-line2", { duration: 0, text: "", ease: "none" })
+
+    .to(".ch1-line1", { duration: 1, text: "je suis le seul survivant", ease: "none" })
+    .to(".ch1-line2", { duration: 1, text: " et je suis en danger", ease: "none" })
+
+    .to(".ch1-line1", { duration: 0, text: "", ease: "none", delay: 0.1 })
+    .to(".ch1-line2", { duration: 0, text: "", ease: "none" })
+
+    .to(".ch1-line1", { duration: 1, text: "Ouvrez la porte", ease: "none" })
+    .to(".ch1-line3", { duration: 0, opacity: 1, ease: "none" })
+    .to(".ch1-line3", { duration: 0.01, scale: 1.2, ease: "none" })
+    .to(".ch1-line3", { duration: 0.01, scale: 1, ease: "none" })
+    .to(".ch1-line3", { duration: 0.01, scale: 1.2, ease: "none" })
+
+    .to(".ch1-line1", { duration: 0, text: "", ease: "none", delay: 0.1 })
+    .to(".ch1-line3", { duration: 0, text: "", ease: "none" })
+
+    //robots
+    .from(".ss-robot-1", { x: "800%", ease: "power1.out", })
+    .from(".ss-robot-2", { x: "800%", ease: "power1.out", delay: -0.49 })
+    .from(".ss-robot-3", { x: "800%", ease: "power1.out", delay: -0.48 })
+
+    //entre en scene marco
+    .from(".ss-m-ch1", { x: "-800%", ease: "none", })
+
+    .to(".marco-course", { duration: 0, opacity: 0, ease: "none", })
+    .to(".marco-pre-saut", { duration: 0, opacity: 1, ease: "none", })
+
+    .to(".ss-m-ch1", { x: "500%", ease: "none", delay: 0.2 })
+    .from(".marco-pre-saut", { duration: 0, opacity: 0, ease: "none", delay: -0.5 })
+    .from(".marco-saut", { duration: 0, opacity: 1, ease: "none", })
     
+
 
 
 //chapitre 2
@@ -79,12 +121,30 @@ gsap.timeline({scrollTrigger: {
     end:'+=3000',
     trigger:'#chapitre2',
 }})
-    .to(".ch2-img1", { duration: 1.5, opacity: 1, ease: "none"})
+    .to(".ch2-img1", { duration: 0, opacity: 1, ease: "none"})
     .to(".num-chap-2", { duration: 1, text: 'Chapitre 2:', ease: "none", delay: 0.1 })
     .to(".nom-chap-2", { duration: 1, text: "Monika", ease: "none", delay: 0.1 })
     .to(".num-chap-2", { duration: 1.5, opacity: 0, ease: "none", delay: 1 })
     .to(".nom-chap-2", { duration: 1.5, opacity: 0, ease: "none", delay: -1.5 })
     .to(".bnc2", { duration: 1.5, opacity: 0, ease: "none", delay: -1.5 })
+
+
+    .to(".ans-19", { duration: 1, text: "19 ans plus tôt", ease: "none" })
+
+
+
+    //switch d'image
+    .to(".ch2-img2", { duration: 0, opacity: 1, ease: "none"})
+    .to(".ch2-img1", { duration: 1, opacity: 0, ease: "none"})
+    
+    .to(".ch2-img3", { duration: 0, opacity: 1, ease: "none"})
+    .to(".ch2-img2", { duration: 1, opacity: 0, ease: "none"})
+    
+    .to(".ch2-img4", { duration: 0, opacity: 1, ease: "none"})
+    .to(".ch2-img3", { duration: 1, opacity: 0, ease: "none"})
+    
+    .to(".ch2-img5", { duration: 0, opacity: 1, ease: "none"})
+    .to(".ch2-img4", { duration: 1, opacity: 0, ease: "none"})
 
 //chapitre 3
 gsap.timeline({scrollTrigger: {
